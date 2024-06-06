c40b2044649a5fee7be64aaed8229b1ba81f89ce
Oteri Prince <oteriprince01@gmail.com>
2024-06-06 11:59:02 +0100

    changes

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import authRouthe from "./route/auth.route.js";
+import authRouth from "./route/auth.route.js";
 import testRoute from "./route/test.route.js";
-import updateUserRoute from "../api/route/user.route.js";
+import updateUserRoute from "./route/user.route.js";
 import postRoute from "./route/post.route.js";
 import chatRoutes from "./route/chat.route.js";
 import messageRoutes from "./route/message.route.js";
@@ -12,7 +12,7 @@ import messageRoutes from "./route/message.route.js";
 const app = express();
 dotenv.config();
 
-app.get("hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("hello there");
 });
 
@@ -20,7 +20,7 @@ app.get("hello", (req, res) => {
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
-app.use("/api/v1", authRouthe);
+app.use("/api/v1", authRouth);
 app.use("/api/v1/test", testRoute);
 app.use("/api/v1/", updateUserRoute);
 app.use("/api/v1/", postRoute);
