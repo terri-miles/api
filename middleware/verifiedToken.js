a2acad6bd353861b6245d0606a3bcf6f46661b48
Oteri Prince <oteriprince01@gmail.com>
2024-06-07 13:55:50 +0100

    updated

--- middleware/verifiedToken.js
+++ middleware/verifiedToken.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
-  const token = req.cookies['access_token'];
-  console.log(token);
+  const token = req.cookies.access_token;
+  console.log(req.cookies);
   if (!token) return res.status(401).json({ message: "Not Authenticated" });
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
